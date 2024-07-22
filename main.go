02cf56ccbbd4a616e90abf58dd41caa546b3d3e6
Chris <sdecuir12@gmail.com>
2024-07-22 13:42:33 -0500

    add metrics reset and fileserverhits funcs

--- main.go
+++ main.go
@@ -1,19 +1,68 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"strconv"
+)
 
 func main() {
 	startServer()
 }
 
 func startServer() {
-	var server http.Server
-	serverHandle := http.NewServeMux()
-	serverHandle.Handle("/", http.FileServer(http.Dir(".")))
-	server.Handler = serverHandle
+	port := "8080"
+	mux := http.NewServeMux()
+	apiCfg := &apiConfig{
+		fileserverHits: 0,
+	}
 
-	server.Addr = "localhost:8080"
+	healthzHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}
 
-	server.ListenAndServe()
+	readinessDir := "/healthz"
+	mux.HandleFunc(readinessDir, healthzHandler)
 
+	filepathRoot := "/app/"
+	dir := http.Dir(".")
+	fileserver := http.FileServer(dir)
+	handler := http.StripPrefix(filepathRoot, apiCfg.middlewareMetricsInc(fileserver))
+	mux.Handle(filepathRoot, handler)
+
+	mux.Handle("/metrics", http.HandlerFunc(apiCfg.metrics))
+	mux.Handle("/reset", http.HandlerFunc(apiCfg.reset))
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) metrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits: " + strconv.Itoa(cfg.fileserverHits)))
+}
+
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits successfully reset"))
 }
